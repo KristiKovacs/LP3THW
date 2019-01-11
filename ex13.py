385805c72f059f2ec49748ebeaae6d7cce5fef86
Kris <payankris@gmail.com>
2019-01-11 11:46:46 -0700

    added

---
+++ ex13.py
@@ -0,0 +1,9 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, stuff, things, that, popcorn = argv
+
+print("The script is called:", script)
+print("Your first variable is:", stuff)
+print("Your second variable is:", things)
+print("Your third variable is:", that)
+print("Your third variable is:", popcorn)
